b6495473cecbcbff76b64d9836f734a441fba1d0
Donard97 <donardgolaj@gmail.com>
2021-08-25 15:47:25 +0200

    Add new js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import completedStatus from './script';
 
 const allTasks = document.getElementById('all-tasks');
 
@@ -24,15 +25,37 @@ const displayTasks = () => {
   allTasks.innerHTML = '';
   for (let i = 0; i < taskList.length; i += 1) {
     const each = taskList[i];
-    const list = `<div class="eachTask">
-      <div class="group-list">
-      <input type="checkbox" class="box" id="list-box" name="list-box">
-        <p class="task-name">${each.description}</p>
-      </div> 
-      <button class="menu-icon" id="${each.id}"><i class="fas fa-ellipsis-v"></i></button>
-    </div>
-    <hr>`;
-    allTasks.innerHTML += list;
+
+    const eachTask = document.createElement('div');
+    eachTask.className = 'eachTask';
+
+    const list = document.createElement('div');
+    list.className = 'group-list';
+
+    const input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('class', 'check-box');
+    input.id = each.id;
+    input.checked = each.completed;
+    input.addEventListener('change', () => {
+      completedStatus(each, taskList);
+    });
+
+    list.appendChild(input);
+
+    const label = document.createElement('label');
+    label.innerHTML = `${each.description}`;
+    label.className = 'form-label';
+    list.appendChild(label);
+
+    eachTask.appendChild(list);
+
+    const button = document.createElement('button');
+    button.innerHTML = '<i class="fas fa-ellipsis-v">';
+    button.className = 'menu-icon';
+    eachTask.appendChild(button);
+
+    allTasks.appendChild(eachTask);
   }
 };
 
