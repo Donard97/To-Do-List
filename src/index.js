3ad537d8dd7496bf469d72206acf538f91e84c1a
Donard97 <donardgolaj@gmail.com>
2021-08-26 11:39:28 +0200

    Update localstorage if statement

--- src/index.js
+++ src/index.js
@@ -25,43 +25,42 @@ let taskList = [
 const displayTasks = () => {
   if (localStorage.getItem('taskList') !== null) {
     taskList = JSON.parse(localStorage.getItem('taskList'));
+  }
+  allTasks.innerHTML = '';
+  for (let i = 0; i < taskList.length; i += 1) {
+    const each = taskList[i];
 
-    allTasks.innerHTML = '';
-    for (let i = 0; i < taskList.length; i += 1) {
-      const each = taskList[i];
+    const eachTask = document.createElement('div');
+    eachTask.className = 'eachTask';
 
-      const eachTask = document.createElement('div');
-      eachTask.className = 'eachTask';
+    const list = document.createElement('div');
+    list.className = 'group-list';
 
-      const list = document.createElement('div');
-      list.className = 'group-list';
+    const input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('class', 'check-box');
+    input.id = each.id;
+    input.checked = each.completed;
+    // eslint-disable-next-line no-loop-func
+    input.addEventListener('change', () => {
+      completedStatus(each, taskList);
+    });
 
-      const input = document.createElement('input');
-      input.setAttribute('type', 'checkbox');
-      input.setAttribute('class', 'check-box');
-      input.id = each.id;
-      input.checked = each.completed;
-      // eslint-disable-next-line no-loop-func
-      input.addEventListener('change', () => {
-        completedStatus(each, taskList);
-      });
+    list.appendChild(input);
 
-      list.appendChild(input);
+    const label = document.createElement('label');
+    label.innerHTML = `${each.description}`;
+    label.className = 'form-label';
+    list.appendChild(label);
 
-      const label = document.createElement('label');
-      label.innerHTML = `${each.description}`;
-      label.className = 'form-label';
-      list.appendChild(label);
+    eachTask.appendChild(list);
 
-      eachTask.appendChild(list);
+    const button = document.createElement('button');
+    button.innerHTML = '<i class="fas fa-ellipsis-v">';
+    button.className = 'menu-icon';
+    eachTask.appendChild(button);
 
-      const button = document.createElement('button');
-      button.innerHTML = '<i class="fas fa-ellipsis-v">';
-      button.className = 'menu-icon';
-      eachTask.appendChild(button);
-
-      allTasks.appendChild(eachTask);
-    }
+    allTasks.appendChild(eachTask);
   }
 };
 
