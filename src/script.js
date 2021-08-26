ea819f8f15047328de94dcdfd9c6f105354972cb
Donard97 <donardgolaj@gmail.com>
2021-08-26 11:31:06 +0200

    Deploy pages with commands

--- src/script.js
+++ src/script.js
@@ -3,7 +3,6 @@ import './style.css';
 const completedStatus = (item, taskList) => {
   item.completed = !item.completed;
   localStorage.setItem('taskList', JSON.stringify(taskList));
-  taskList = JSON.parse(localStorage.getItem('taskList'));
 };
 
 export default completedStatus;
