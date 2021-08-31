ec19196539c1ce55e3c042e21d1c17b928f2fc52
Donard97 <donardgolaj@gmail.com>
2021-08-31 16:52:22 +0200

    Add tests for remove items, update storage and completed tasks

--- src/tests/remove-items-mocks.test.js
+++ src/tests/remove-items-mocks.test.js
@@ -1,5 +1,5 @@
 import localStorageMock from '../mocks/local-storage-mock';
-import removeTask from '../mocks/remove-items-mocks';
+import { removeTask, removeCompleted } from '../mocks/remove-items-mocks';
 
 describe('Delete One Task', () => {
   const listTask = [
@@ -38,5 +38,46 @@ describe('Delete One Task', () => {
 
   it('Return updated array', () => {
     expect(removeTask(listTask, 0)).toEqual([{ completed: false, description: 'Start Microverse at 9 am', id: 1 }]);
+  });
+});
+
+describe('Delete All Completed', () => {
+  const linkTask = [
+    {
+      id: 1,
+      description: 'Task 1',
+      completed: false,
+    },
+    {
+      id: 2,
+      description: 'Task 2',
+      completed: true,
+    },
+    {
+      id: 3,
+      description: 'Task 3',
+      completed: true,
+    },
+    {
+      id: 4,
+      description: 'Task 4',
+      completed: false,
+    },
+  ];
+
+  it('Delete completed tasks', () => {
+    expect(removeCompleted(linkTask)).toHaveLength(2);
+  });
+
+  it('Update local storage after deleting', () => {
+    expect(localStorageMock.getItem('taskList')[1].description).toBe('Task 4');
+  });
+
+  it('Update local storage after deleting', () => {
+    expect(localStorageMock.getItem('taskList')[1].id).toBe(2);
+  });
+
+  it('Index of task 0 should be 1', () => {
+    expect(removeCompleted(linkTask)[0].id).toBe(1);
   });
 });
